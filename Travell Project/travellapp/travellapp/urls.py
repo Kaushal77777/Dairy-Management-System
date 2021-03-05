a6f3ff08aaae2f4be8aac84d98772d6f7853a9e4
Kaushal77777 <patelkaushal736@gmail.com>
2021-03-05 09:49:02 +0530

    New Login and Register is connected to app

--- Travell Project/travellapp/travellapp/urls.py
+++ Travell Project/travellapp/travellapp/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('tourism.urls')),
     path('admin/', admin.site.urls),
+    path('reglog/',include('reglog.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
