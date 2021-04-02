ed1565df548b1f63f485bf2278963abec553b498
Kaushal77777 <patelkaushal736@gmail.com>
2021-04-02 15:43:16 +0530

    Little Changes are done for testing

--- SP Project/travellapp/tourism/tests.py
+++ SP Project/travellapp/tourism/tests.py
@@ -7,6 +7,7 @@ import time
 class TestProjectListPage(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome('chromedriver.exe')
+		#self.browser = webdriver.Firefox()
 
 	def tearDown(self):
 		self.browser.close()
@@ -19,11 +20,11 @@ class TestProjectListPage(StaticLiveServerTestCase):
 		link = self.browser.find_element_by_link_text('Login')
 		link.click()
 
-		self.browser.find_element_by_name("username").send_keys("jenil1")
+		self.browser.find_element_by_name("username").send_keys("d123")
 		time.sleep(1)
-		self.browser.find_element_by_name("password").send_keys("123456")
+		self.browser.find_element_by_name("password").send_keys("123")
 		time.sleep(1)
-		self.browser.find_element_by_name("submit").click()
+		self.browser.find_element_by_name("login").click()
 		time.sleep(5)
 
 	def register_test(self):
@@ -34,19 +35,19 @@ class TestProjectListPage(StaticLiveServerTestCase):
 		link = self.browser.find_element_by_link_text('Register')
 		link.click()
 
-		self.browser.find_element_by_name("first_name").send_keys("jenil")
+		self.browser.find_element_by_name("first_name").send_keys("Dhruvit")
 		time.sleep(1)
-		self.browser.find_element_by_name("last_name").send_keys("gajera")
+		self.browser.find_element_by_name("last_name").send_keys("Ramani")
 		time.sleep(1)
-		self.browser.find_element_by_name("username").send_keys("jenil2")
+		self.browser.find_element_by_name("username").send_keys("d123")
 		time.sleep(1)
-		self.browser.find_element_by_name("email").send_keys("j1@gmail.com")
+		self.browser.find_element_by_name("email").send_keys("d@gmail.com")
 		time.sleep(1)
-		self.browser.find_element_by_name("password1").send_keys("123456")
+		self.browser.find_element_by_name("password1").send_keys("123")
 		time.sleep(1)
-		self.browser.find_element_by_name("password2").send_keys("123456")
+		self.browser.find_element_by_name("password2").send_keys("123")
 		time.sleep(1)
-		self.browser.find_element_by_name("submit").click()
+		self.browser.find_element_by_name("register").click()
 		time.sleep(5)
 
 
